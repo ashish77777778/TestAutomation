c69cbcce12c0b5cb876d20a984b748315fb1ac38
ashish77777778 <41377453+ashish77777778@users.noreply.github.com>
2020-07-25 23:22:50 +0530

    Update Testbase.java

--- src/main/java/com/automation/qa/base/Testbase.java
+++ src/main/java/com/automation/qa/base/Testbase.java
@@ -15,8 +15,8 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+//import org.testng.annotations.AfterTest;
+//import org.testng.annotations.BeforeTest;
 
 import com.automation.qa.util.TestUtil;
 import com.relevantcodes.extentreports.ExtentReports;
